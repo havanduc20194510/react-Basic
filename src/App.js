3adcd500b18667e3dadf69921c9e5b4207f9bd83
HaDuciuiu <havanducdeptrai@gmail.com>
2021-10-11 15:57:45 +0700

    init project reactjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world with reactjs (Ha Van Duc)
         </p>
         <a
           className="App-link"
